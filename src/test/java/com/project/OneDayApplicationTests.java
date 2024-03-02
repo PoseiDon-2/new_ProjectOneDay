5156110752cd6f14a87485141ef428f26120d59e
PoseiDon <suriya.b@kkumail.com>
2024-03-02 20:56:05 +0700

    new

---
+++ src/test/java/com/project/OneDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
